20988ed6948177552709bacbd1b5b1ef1f63561e
RockfordWei <rocky@perfect.org>
2018-02-07 12:59:46 -0500

    Cleaning up

--- iss-dl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-curl -H "X-ASESSIONID: $3" \
-"X-AUSERNAME: $2" \
--b jiracookie.txt \
-"http://jira.perfect.org:8080/si/jira.issueviews:issue-xml/ISS-$1/ISS-$1.xml" \
-> ISS-$1.xml
--- jiralogin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-clear
-curl -vX POST -d "os_username=$1&os_password=$2" \
--c jiracookie.txt \
-"http://jira.perfect.org:8080/login.jsp"
-#for i in $(seq 1 586); do ./iss-dl.sh $i; done
